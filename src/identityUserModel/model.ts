8f4eec0ff0f30bb3ba13111a6c46f1a788f0e24d
vichi2323 <shabanche@gmail.com>
2023-02-11 19:28:43 +0100

    commit from local machine

--- src/identityUserModel/model.ts
+++ src/identityUserModel/model.ts
@@ -8,6 +8,20 @@ export class IUser {
     gender?: string
     address?: string
     active?: boolean
-    createdAt?: string
 
 }
+
+export class IUsers {
+    id?: any
+    firstName?: string
+    lastName?: string
+    birthDate?: string
+    email?: string
+    phone?: string
+    gender?: string
+    address?: string
+    active?: boolean
+
+}
+
+
